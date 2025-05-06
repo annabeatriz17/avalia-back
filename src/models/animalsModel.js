3d2aba8dc6b616f3251f2b810043ddb33d7d919d
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-05-06 08:37:00 -0300

    Update animalsModel.js

---
+++ src/models/animalsModel.js
@@ -0,0 +1,36 @@
+const pool = require("../config/database");
+
+const getAllAnimals = async () => {
+    const result = await pool.query("SELECT * FROM animals");
+    return result.rows;
+};
+
+const getAnimalById = async (id) => {
+    const result = await pool.query("SELECT * FROM animals WHERE id = $1", [id]);
+    return result.rows[0];
+};
+
+const createAnimal = async (name, pesoReal) => {
+    const result = await pool.query("INSERT INTO animals (name, peso_real) VALUES ($1, $2) RETURNING *", [name, pesoReal]);
+    return result.rows[0];
+};
+
+const updateAnimal = async (id, name, pesoReal) => {
+    const result = await pool.query("UPDATE animals SET name = $1, peso_real = $2 WHERE id = $3 RETURNING *", [name, pesoReal, id]
+    );
+    if (result.rowCount === 0) {
+        return { error: "Animal não encontrado" };
+    }
+    return result.rows[0];
+};
+
+const deleteAnimal = async (id) => {
+    const result = await pool.query("DELETE FROM animals WHERE id = $1 RETURNING *", [id]
+    );
+    if (result.rowCount === 0) {
+        return { error: "Não foi possível deletar o animal" };
+    }
+    return { message: "Animal deletado com sucesso" };
+};
+
+module.exports = { getAllAnimals, getAnimalById, createAnimal, updateAnimal, deleteAnimal };
