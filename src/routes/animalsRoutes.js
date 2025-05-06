6f3f1a9e64af42650614216d22147d853ba60fbe
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-05-06 14:34:21 -0300

    update

--- src/routes/animalsRoutes.js
+++ src/routes/animalsRoutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const animalsController = require("../controllers/animalsController");
 const upload = require("../config/upload");
+const apiKeyMiddleware = require("../config/apiKey"); // 🔐
+
+router.use(apiKeyMiddleware);
+
 
 router.get("/animals", animalsController.getAllAnimals);
 router.get("/animals/:id", animalsController.getAnimalById);
--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const reportController = require("../controllers/reportController");
+const apiKeyMiddleware = require("../config/apiKey"); // 🔐
+
+router.use(apiKeyMiddleware);
+
 
 router.get("/report/pdf", reportController.exportAnimalsPDF);
 
